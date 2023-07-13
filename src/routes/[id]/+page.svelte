7bb1c0512908b52e7fff5363a34424a96e68ace9
bnierimi <shemiradiance@gmail.com>
2023-07-13 04:07:28 +0100

    (bnierimi): Added a markdown renderer `svelte-markdown` and @tailwindcss/typography`

--- src/routes/[id]/+page.svelte
+++ src/routes/[id]/+page.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
   import { goto, afterNavigate } from "$app/navigation"
   import { base } from "$app/paths";
-    import Mklogo from "$lib/Trogg/Mklogo.svelte";
-    import Yuzaname from "$lib/Trogg/Yuzaname.svelte";
+  import Mklogo from "$lib/Trogg/Mklogo.svelte";
+  import Yuzaname from "$lib/Trogg/Yuzaname.svelte";
+
+  import SvelteMarkdown from 'svelte-markdown'
 
   let previousPage: string = base;
 
@@ -12,6 +14,23 @@
 
   // to go back
   // goto(previousPage)
+
+
+  const source = `
+  # This is a header
+
+This is a paragraph.
+
+* This is a list
+* With two items
+  1. And a sublist
+  2. That is ordered
+    * With another
+    * Sublist inside
+
+| And this is | A table |
+|-------------|---------|
+| With two    | columns |`
 
   const focusStyle = "focus:outline-none focus:ring-1 focus:ring-emerald-500/70"
 </script>
@@ -42,8 +61,8 @@
       </div>
     </div>
     <hr class="border-zinc-300/50 dark:border-zinc-700/50">
-    <article>
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti quos exercitationem maxime officia, quas magnam nostrum quaerat nesciunt dignissimos. Ratione iure est reiciendis sed excepturi amet pariatur delectus soluta! Vel?
-    </article>
+    <div class="overflow-y-auto prose">
+      <SvelteMarkdown {source} />
+    </div>
   </div>
 </main>
---
+++ src/wknotes/0002 - Downloading a website with wget.md
@@ -0,0 +1,7 @@
+<!-- 
+    Title: <title of the post>
+    Author: <your name here>
+    Published: <date in YYYY-MM-DD HH:MM format>
+    Categories: <comma-separated list of categories>
+    Thumbnail: <url, e.g. images/image.webp then put that image in the static/images folder>
+-->
